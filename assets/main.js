dfaf3be74c7b818f1eba2195259b0150ab1b977e
Martin Klíč <126399810+maklicek@users.noreply.github.com>
2025-10-27 18:40:06 +0100

    Update main.js

--- assets/main.js
+++ assets/main.js
@@ -1,1 +1,9 @@
+const navToggle = document.querySelector(".nav-toggle");
+const navMenu = document.querySelector(".site-nav");
+
+if (navToggle && navMenu) {
+  navToggle.addEventListener("click", () => {
+    navMenu.classList.toggle("open");
+  });
+}
 
